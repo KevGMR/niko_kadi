648dbde4d339b7f5ebf03923b8e1cc99c69ba42c
Kevin Gatamu <kevingatamumuthoni@gmail.com>
2024-10-15 09:56:43 +0300

    update

---
+++ src/data/deck.js
@@ -0,0 +1,273 @@
+export const deck = [
+    {
+      "suit": "Hearts",
+      "rank": "A",
+      "ability": ""
+    },
+    {
+      "suit": "Hearts",
+      "rank": "K",
+      "ability": ""
+    },
+    {
+      "suit": "Hearts",
+      "rank": "Q",
+      "ability": ""
+    },
+    {
+      "suit": "Hearts",
+      "rank": "J",
+      "ability": ""
+    },
+    {
+      "suit": "Hearts",
+      "rank": "10",
+      "ability": ""
+    },
+    {
+      "suit": "Hearts",
+      "rank": "9",
+      "ability": ""
+    },
+    {
+      "suit": "Hearts",
+      "rank": "8",
+      "ability": ""
+    },
+    {
+      "suit": "Hearts",
+      "rank": "7",
+      "ability": ""
+    },
+    {
+      "suit": "Hearts",
+      "rank": "6",
+      "ability": ""
+    },
+    {
+      "suit": "Hearts",
+      "rank": "5",
+      "ability": ""
+    },
+    {
+      "suit": "Hearts",
+      "rank": "4",
+      "ability": ""
+    },
+    {
+      "suit": "Hearts",
+      "rank": "3",
+      "ability": ""
+    },
+    {
+      "suit": "Hearts",
+      "rank": "2",
+      "ability": ""
+    },
+    {
+      "suit": "Diamonds",
+      "rank": "A",
+      "ability": ""
+    },
+    {
+      "suit": "Diamonds",
+      "rank": "K",
+      "ability": ""
+    },
+    {
+      "suit": "Diamonds",
+      "rank": "Q",
+      "ability": ""
+    },
+    {
+      "suit": "Diamonds",
+      "rank": "J",
+      "ability": ""
+    },
+    {
+      "suit": "Diamonds",
+      "rank": "10",
+      "ability": ""
+    },
+    {
+      "suit": "Diamonds",
+      "rank": "9",
+      "ability": ""
+    },
+    {
+      "suit": "Diamonds",
+      "rank": "8",
+      "ability": ""
+    },
+    {
+      "suit": "Diamonds",
+      "rank": "7",
+      "ability": ""
+    },
+    {
+      "suit": "Diamonds",
+      "rank": "6",
+      "ability": ""
+    },
+    {
+      "suit": "Diamonds",
+      "rank": "5",
+      "ability": ""
+    },
+    {
+      "suit": "Diamonds",
+      "rank": "4",
+      "ability": ""
+    },
+    {
+      "suit": "Diamonds",
+      "rank": "3",
+      "ability": ""
+    },
+    {
+      "suit": "Diamonds",
+      "rank": "2",
+      "ability": ""
+    },
+    {
+      "suit": "Clubs",
+      "rank": "A",
+      "ability": ""
+    },
+    {
+      "suit": "Clubs",
+      "rank": "K",
+      "ability": ""
+    },
+    {
+      "suit": "Clubs",
+      "rank": "Q",
+      "ability": ""
+    },
+    {
+      "suit": "Clubs",
+      "rank": "J",
+      "ability": ""
+    },
+    {
+      "suit": "Clubs",
+      "rank": "10",
+      "ability": ""
+    },
+    {
+      "suit": "Clubs",
+      "rank": "9",
+      "ability": ""
+    },
+    {
+      "suit": "Clubs",
+      "rank": "8",
+      "ability": ""
+    },
+    {
+      "suit": "Clubs",
+      "rank": "7",
+      "ability": ""
+    },
+    {
+      "suit": "Clubs",
+      "rank": "6",
+      "ability": ""
+    },
+    {
+      "suit": "Clubs",
+      "rank": "5",
+      "ability": ""
+    },
+    {
+      "suit": "Clubs",
+      "rank": "4",
+      "ability": ""
+    },
+    {
+      "suit": "Clubs",
+      "rank": "3",
+      "ability": ""
+    },
+    {
+      "suit": "Clubs",
+      "rank": "2",
+      "ability": ""
+    },
+    {
+      "suit": "Spades",
+      "rank": "A",
+      "ability": ""
+    },
+    {
+      "suit": "Spades",
+      "rank": "K",
+      "ability": ""
+    },
+    {
+      "suit": "Spades",
+      "rank": "Q",
+      "ability": ""
+    },
+    {
+      "suit": "Spades",
+      "rank": "J",
+      "ability": ""
+    },
+    {
+      "suit": "Spades",
+      "rank": "10",
+      "ability": ""
+    },
+    {
+      "suit": "Spades",
+      "rank": "9",
+      "ability": ""
+    },
+    {
+      "suit": "Spades",
+      "rank": "8",
+      "ability": ""
+    },
+    {
+      "suit": "Spades",
+      "rank": "7",
+      "ability": ""
+    },
+    {
+      "suit": "Spades",
+      "rank": "6",
+      "ability": ""
+    },
+    {
+      "suit": "Spades",
+      "rank": "5",
+      "ability": ""
+    },
+    {
+      "suit": "Spades",
+      "rank": "4",
+      "ability": ""
+    },
+    {
+      "suit": "Spades",
+      "rank": "3",
+      "ability": ""
+    },
+    {
+      "suit": "Spades",
+      "rank": "2",
+      "ability": ""
+    },
+    {
+      "suit": "Joker",
+      "rank": "Joker 1",
+      "ability": ""
+    },
+    {
+      "suit": "Joker",
+      "rank": "Joker 2",
+      "ability": ""
+    }
+  ]
+
---
+++ src/pages/Login.jsx
@@ -0,0 +1,15 @@
+
+
+function Login() {
+
+  const user = localStorage.getItem("user")
+
+  console.log(user);
+  
+
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
